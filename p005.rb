0f2ac44b436ea2aeedc806dd432983b3f511400e
Uvero <uvero3@gmail.com>
2016-08-22 14:01:24 +0300

    solved problem 5

---
+++ p005.rb
@@ -0,0 +1,11 @@
+#2520 is the smallest number that can be divided by
+#each of the numbers from 1 to 10 without any remainder.
+
+#What is the smallest positive number that is
+#evenly divisible by all of the numbers from 1 to 20?
+
+require './common.rb'
+solve do
+  rng = (1..20).to_a
+  lcm *rng
+end
