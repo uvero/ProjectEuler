e44f10bff87f5d34e84c3f0464134db1c730634d
Uvero <uvero3@gmail.com>
2016-09-11 18:52:49 +0300

    solved p018_bruteforce.rb

---
+++ p018_bruteforce.rb
@@ -0,0 +1,43 @@
+#By starting at the top of the triangle below
+#and moving to adjacent numbers on the row below,
+#Find the maximum total from top to bottom of the triangle
+
+#the same solution from problem 67 would work here, and in better time,
+#but here the brute-force solution is presented, as an example
+#to "encoding" the paths as numbers.
+
+require './common.rb'
+
+def sum_path(tree,num)
+  j = 0
+  sum = 0
+  for i in 0...(tree.length)
+    sum += tree[i][j]
+    j += num%2
+    num /= 2
+  end
+  sum
+end
+
+str = <<STR
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+STR
+tree = str.split("\n").map{ |x| x.split(" ").map(&:to_i) }
+lim = 2**(tree.length)
+solve do
+  (0...lim).map{ |path| sum_path(tree,path) }.max
+end
